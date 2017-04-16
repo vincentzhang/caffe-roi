61b5d6ddfe31914555669b47bb980073fc954176
Vincent Zhang <vzhangca@gmail.com>
2017-04-16 00:11:10 -0600

    scaffolding a new roi_conv_layer

---
+++ include/caffe/layers/roi_conv_layer.hpp
@@ -0,0 +1,43 @@
+#ifndef CAFFE_ROI_CONV_LAYER_HPP_
+#define CAFFE_ROI_CONV_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/base_conv_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+class ROIConvolutionLayer : public BaseConvolutionLayer<Dtype> {
+ public:
+  explicit ROIConvolutionLayer(const LayerParameter& param)
+      : BaseConvolutionLayer<Dtype>(param) {}
+
+  virtual inline const char* type() const { return "ROIConvolution"; }
+  
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  virtual inline bool EqualNumBottomTopBlobs() const { return false; }
+
+ protected:
+  //virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  //virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return false; }
+  virtual void compute_output_shape();
+
+  Dtype spatial_scale_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_ROI_CONV_LAYER_HPP_
